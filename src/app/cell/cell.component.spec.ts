670b971ec7e7cbdd50a62c9f26deb749f8e1da1d
Jeremy Likness <jeremy@jeremylikness.com>
2016-07-29 15:18:18 -0400

    cell component tests

--- src/app/cell/cell.component.spec.ts
+++ src/app/cell/cell.component.spec.ts
@@ -1,13 +1,77 @@
 /* tslint:disable:no-unused-variable */
 
 import { By }           from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
-import { describe, it, expect, async, inject } from '@angular/core/testing';
+import { DebugElement, ElementRef } from '@angular/core';
+import { beforeEach, describe, it, expect, async, inject } from '@angular/core/testing';
 import { CellComponent } from './cell.component';
+import { Room } from '../world/room';
+import { Directions } from '../world/directions';
+import { 
+  NOT_VISITED_COLOR, 
+  VISITED_COLOR,
+  CURRENT_COLOR,
+  WALL_COLOR
+} from '../settings';
 
 describe('Component: Cell', () => {
+  let element: ElementRef = null; 
+  let div = null;
+  let room: Room = null;
+  let component: CellComponent = null;
   it('should create an instance', () => {
-    let component = new CellComponent();
+    component = new CellComponent();
     expect(component).toBeTruthy();
+  });
+  describe('processStyle', () => {
+    beforeEach(() => {
+      room = new Room(); 
+      div = {
+        style: {
+          background: null,
+          borderLeftColor: null,
+          borderTopColor: null,
+          borderBottomColor: null,
+          borderRightColor: null
+        }
+      };
+      element = {
+        nativeElement: div 
+      };
+      component = new CellComponent();
+    });
+
+    it ('should set background to the not visited color', () => {
+      component.room = room; 
+      component.cellDiv = element;
+      expect(div.style.background).toBe(NOT_VISITED_COLOR);
+    });
+
+    it('should set background to current color when room is current', () => {
+      room.visited = true;
+      component.room = room;
+      component.isCurrentRoom = true;
+      component.cellDiv = element;
+      expect(div.style.background).toBe(CURRENT_COLOR);
+    });
+
+    it('should set background to visited color when room has been visited', () => {
+      room.visited = true;
+      component.room = room;
+      component.cellDiv = element;
+      expect(div.style.background).toBe(VISITED_COLOR);
+    });
+
+    it('should not set the wall color for directions that are not walls', () => {
+      room.visited = true;
+      room.setDirection(Directions.West, room);
+      room.setDirection(Directions.South, room);
+      component.room = room;
+      component.cellDiv = element;
+      expect(div.style.borderTopColor).toBe(WALL_COLOR);
+      expect(div.style.borderRightColor).toBe(WALL_COLOR);
+      expect(div.style.borderLeftColor).toBe(null);
+      expect(div.style.borderBottomColor).toBe(null);
+    });
+
   });
 });
