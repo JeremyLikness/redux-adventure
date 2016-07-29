fcab94e4494f8ccfffa7f10e7e337d343c9db1e3
Jeremy Likness <jeremy@jeremylikness.com>
2016-07-29 10:29:33 -0400

    Simplify room reducer

--- src/app/reducers/reducer.room.ts
+++ src/app/reducers/reducer.room.ts
@@ -6,31 +6,19 @@ import { ACTION_GET, ACTION_MOVE } from '../actions/ActionList';
 
 export const room = (state: Room = new Room(), action: IAction) => {
 
-    if (action.type === ACTION_GET) {
-        let inventoryAction = action as IInventoryAction;
-        let room = new Room();
-        room.idx = state.idx;
-        room.visited = state.visited;
-        room.directions = [...state.directions];
-        room.walls = [...state.walls];
-        room.name = state.name; 
-        room.description = state.description; 
-        room.things = things(state.things, action); 
-        return room; 
-    }
-
+    let room = new Room();
+    room.idx = state.idx;
+    room.directions = [...state.directions];
+    room.walls = [...state.walls];
+    room.name = state.name; 
+    room.description = state.description; 
+    room.visited = state.visited;
+    room.things = action.type === ACTION_GET ? things(state.things, action) : [...state.things]; 
+    
     if (action.type === ACTION_MOVE && (<IRoomAction>action).newRoom.visited === false) {
-        let room = new Room();
-        room.idx = state.idx;
         room.visited = true;
-        room.directions = [...state.directions];
-        room.walls = [...state.walls];
-        room.name = state.name; 
-        room.description = state.description; 
-        room.things = [...state.things]; 
-        return room;
     }
 
-    return state;
+    return room;
 
 }
